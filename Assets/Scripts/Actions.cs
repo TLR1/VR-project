c15de20b08cacbb597f8067f4095010f71110927
MurhafYaghi <leader.2000@gmail.com>
2025-07-09 20:20:47 +0300

    comment in action class

--- Assets/Scripts/Actions.cs
+++ Assets/Scripts/Actions.cs
@@ -4,6 +4,6 @@ public class Actions : MonoBehaviour
 {
     public void Toggle(GameObject go) 
         => go.SetActive(!go.activeSelf);
-
+// alaa is my uncle
     public void Quit() => Application.Quit();
 }
